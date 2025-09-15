ee92802df59b6a9b2a8a38139c4958019730b0b9
marwin27 <abaquinmarwinc@gmail.com>
2025-09-16 04:09:54 +0800

    Update backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,46 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const ExistingUser = await User.findOne({ email });
+
+    if (ExistingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = new User({
+      name,
+      email,
+      password,
+    });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error registering user", error });
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/");
+    console.log("Connected to MongoDB");
+
+    app.listen(3005, () => {
+      console.log("Server started on port 3005");
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
